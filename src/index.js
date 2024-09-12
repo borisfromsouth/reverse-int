3cb0f5a8cc722aedca7c7e530e63023b222b7e24
Alexander Borisenko <sashaborisenko@tut.by>
2024-09-12 13:56:46 +0300

    fix: add code to correct place

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
+module.exports = function reverse (number) {
+    let numLeft = 0;
+    let res = 0;
+
+    number = Math.abs(number);
+    while(number){
+        numLeft = number % 10;
+        res = (res * 10) + numLeft;
+        number = number / 10 | 0;
+    }
   
+    return res;
 }
